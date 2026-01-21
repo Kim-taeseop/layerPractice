5af3d257bd39768f908670283ec22d673bab0b63
Kim tae-seop <kts3445@naver.com>
2026-01-21 16:51:05 +0900

    feat : 프로젝트 생성

---
+++ src/main/java/practice/layerPractice/LayerPracticeApplication.java
@@ -0,0 +1,13 @@
+package practice.layerPractice;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class LayerPracticeApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(LayerPracticeApplication.class, args);
+	}
+
+}
---
+++ src/test/java/practice/layerPractice/LayerPracticeApplicationTests.java
@@ -0,0 +1,13 @@
+package practice.layerPractice;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class LayerPracticeApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
