f66b209164f4930b7af991102c6653155a5d88a9
Kim tae-seop <kts3445@naver.com>
2026-01-23 16:00:01 +0900

    feat : mysql 데이터베이스 연결

--- src/main/java/practice/layerPractice/entity/HandEntity.java
+++ src/main/java/practice/layerPractice/entity/HandEntity.java
@@ -1,12 +1,16 @@
 package practice.layerPractice.entity;
 
+import jakarta.persistence.Entity;
+import jakarta.persistence.Id;
 import lombok.Getter;
 import lombok.Setter;
 
 @Getter
 @Setter
+@Entity
 public class HandEntity {
 
+    @Id
     private Long id;
     private String name;
 }
--- src/main/java/practice/layerPractice/repository/HandRepository.java
+++ src/main/java/practice/layerPractice/repository/HandRepository.java
@@ -1,5 +1,6 @@
 package practice.layerPractice.repository;
 
+import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 import practice.layerPractice.entity.HandEntity;
 
@@ -7,7 +8,7 @@ import java.util.List;
 import java.util.Optional;
 
 @Repository
-public interface HandRepository {
+public interface HandRepository extends JpaRepository<HandEntity, Long> {
 
     // 저장
     HandEntity save(HandEntity handEntity);
