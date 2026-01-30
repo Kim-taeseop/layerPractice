c122709113666fdca86cddd84b559ef6273a157f
Kim tae-seop <kts3445@naver.com>
2026-01-30 19:47:00 +0900

    feat : 임베디드 타입 설명 변경

--- src/main/java/practice/layerPractice/entity/description/Period.java
+++ src/main/java/practice/layerPractice/entity/description/Period.java
@@ -9,6 +9,8 @@ import java.time.LocalDateTime;
 Base Entity와 유사하지만
 임베디드는 엔티티 안의 값 타입
 베이스 엔티티는 엔티티의 공통 뼈대
+임베디드 타입 같은 값 타입을 여러 엔티티가 공유하면 위험함. 한쪽에서 변경하면 다른엔티티도 변경
+따라서 값을 복사해서 사용
  */
 @Embeddable
 public class Period {
