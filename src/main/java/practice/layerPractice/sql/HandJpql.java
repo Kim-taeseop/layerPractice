83e775a1da5d1bb6a0a3dd43d03a77c3c144d084
Kim tae-seop <kts3445@naver.com>
2026-02-02 21:13:42 +0900

    feat : 프로젝션, 페이징 내용 추가

--- src/main/java/practice/layerPractice/sql/HandJpql.java
+++ src/main/java/practice/layerPractice/sql/HandJpql.java
@@ -5,10 +5,11 @@ jpql 쿼리 메모용 클래스
  */
 
 import jakarta.persistence.EntityManager;
+import jakarta.persistence.Query;
 import jakarta.persistence.TypedQuery;
-import practice.layerPractice.sql.sqlEntity.MemberSQL;
-import practice.layerPractice.sql.sqlEntity.TeamSQL;
+import practice.layerPractice.sql.sqlEntity.*;
 
+import java.lang.reflect.Member;
 import java.util.List;
 
 public class HandJpql {
@@ -25,15 +26,33 @@ public class HandJpql {
      */
 
     // List 찾기
-    String listSql1 = "select m From MemberSQL m where m.name like '%hello%'";
-    String listSql2 = "select m From MemberSQL m where m.age > 20";
+    String listSql1 = "select distinct m from MemberSQL m where m.name like '%hello%'";  // 조건부 like, distinct : 중복제거
+    String listSql2 = "select m from MemberSQL m where m.age > 20"; // 조건부 비교
     List<MemberSQL> listResult = em.createQuery(listSql1, MemberSQL.class).getResultList();
+
+    Query query1 = em.createQuery("select m.name, m.age from MemberSQL m");  // 스칼라 타입 방식1
+    List<MemberDTO> memberDTOResult = em.createQuery(
+            "select new practice.layerPractice.sql.sqlEntity.MemberDTO(m.name, m.age) from MemberSQL m"
+                    , MemberDTO.class).getResultList(); // 스칼라 타입 방식2  DTO 사용
+
     List<TeamSQL> listTeam = em.createQuery(
             "select t from MemberSQL m join m.teamSQL t", TeamSQL.class).getResultList();    // 조인 쿼리
 
+    List<AddressSQL> orders = em.createQuery(
+            "select o.addressSQL from OrderSQL o", AddressSQL.class).getResultList();   // 임베디드 사용
+
+
+    // 페이징 setFirstResult(int start) : 조회 시작 위치  setMaxResults(int max) : 조회할 데이터 수
+    List<MemberSQL> pagingResult = em.createQuery(
+            "select m from MemberSQL m order by m.age desc", MemberSQL.class)
+            .setFirstResult(0).setMaxResults(10)    // 0번째 부터 10개 들고옴
+            .getResultList();
+
+
+
     // 단일값 찾기
     MemberSQL singleResult = em.createQuery(
-            "select m from MemeberSQL m where m.name = :name", MemberSQL.class)
+            "select m from MemberSQL m where m.name = :name", MemberSQL.class)
             .setParameter("name", "member1")    // 파라미터 바인딩
             .getSingleResult();
 
---
+++ src/main/java/practice/layerPractice/sql/sqlEntity/MemberDTO.java
@@ -0,0 +1,11 @@
+package practice.layerPractice.sql.sqlEntity;
+
+
+/*
+jpql 사용을 위한 DTO
+ */
+public class MemberDTO {
+
+    private String name;
+    private int age;
+}
