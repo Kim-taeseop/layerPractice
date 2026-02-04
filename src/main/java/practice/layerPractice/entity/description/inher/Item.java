d1c3409c6badaf87f4da7f02e4aedd02c6be69ff
Kim tae-seop <kts3445@naver.com>
2026-02-04 19:41:18 +0900

    feat : Item Getter, Setter 추가

--- src/main/java/practice/layerPractice/entity/description/inher/Item.java
+++ src/main/java/practice/layerPractice/entity/description/inher/Item.java
@@ -1,6 +1,8 @@
 package practice.layerPractice.entity.description.inher;
 
 import jakarta.persistence.*;
+import lombok.Getter;
+import lombok.Setter;
 
 /*
 상속관계 매핑을 위한 엔티티 Item / Book
@@ -11,6 +13,7 @@ strategy = InheritanceType.SINGLE_TABLE : 통합 테이블
 strategy = InheritanceType.TABLE_PER_CLASS : 구현 클래스 마다 테이블  // 사실 잘 안씀.
  */
 @Entity
+@Getter @Setter
 @Inheritance(strategy = InheritanceType.JOINED)
 @DiscriminatorColumn    // 이 어노테이션을 하면 Item 테이블에 DTYPE 이라는 상속된 엔티티 명이 들어감 (name = "")으로 변경 가능
 public class Item {
